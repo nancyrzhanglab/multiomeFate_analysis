08b1e66da5e6ec986da566d9e8a41ae9c12c5bf7
linnykos <klin1234@gmail.com>
2022-03-24 11:21:43 -0400

    Pushing latest attempts

--- kevin/Writeup4b/Writeup4b_time0time10_saver.R
+++ kevin/Writeup4b/Writeup4b_time0time10_saver.R
@@ -14,12 +14,13 @@ jackpot_genes <- c("SOX10", "MITF", "FN1", "AXL", "EGFR", "NT5E",
                    "SERPINE2", "NDRG1", "FEZF1", "EGR3", "VGF",
                    "WNT5A", "POSTN", "PDGFRB", "NRG1", "VEGFC", "FOSL1",
                    "RUNX2", "LOXL2", "JUN", "PDGFRC")
-jackpot_genes <- intersect(jackpot_genes, rownames(all_data2))
-all_data2[["RNA"]]@var.features <- unique(c(jackpot_genes, all_data2[["RNA"]]@var.features))
+jackpot_genes <- intersect(jackpot_genes, rownames(all_data))
+all_data[["RNA"]]@var.features <- unique(c(jackpot_genes, all_data[["RNA"]]@var.features))
+print(length(all_data[["RNA"]]@var.features))
 
 mat <- all_data[["RNA"]]@counts[Seurat::VariableFeatures(all_data, assay = "RNA"),]
 print(dim(mat))
-saver_res <- SAVER::saver(x = mat, ncores = 1)
+saver_res <- SAVER::saver(x = mat, ncores = 4)
 save(saver_res, date_of_run, session_info,
      file = "../../../../out/kevin/Writeup4b/Writeup4b_time0time10_saver.RData")
 
