f2402666c3067f9440b2f32f404abaa49999faa3
linnykos <klin1234@gmail.com>
2022-05-24 18:15:14 -0400

    Pushing barcode plots

---
+++ kevin/Writeup4e/barcodePlot_functions.R
@@ -0,0 +1,9 @@
+barplot_function <- function(lineage_name,
+                             lineage_mat,
+                             membership_vec
+                             umap_mat,
+                             col_indicator = rgb(1, 0.1, 0.1, 0.2),
+                             col_palette_enrichment = grDevices::colorRampPalette(c('lightgrey', 'blue'))(100),
+                             mode = c("count", "enrichment")){
+  
+}
