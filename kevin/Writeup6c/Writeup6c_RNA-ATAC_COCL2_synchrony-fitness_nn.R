a137136b1e2780ea361e40a733731ee68b0a9f74
linnykos <klin1234@gmail.com>
2023-02-27 19:35:15 -0500

    Pushing fitness scan

---
+++ kevin/Writeup6c/Writeup6c_RNA-ATAC_COCL2_synchrony-fitness_nn.R
@@ -0,0 +1,151 @@
+rm(list=ls())
+library(Seurat)
+library(Signac)
+load("../../../../out/kevin/Writeup6c/Writeup6c_tcca_RNA-ATAC_COCL2.RData")
+
+date_of_run <- Sys.time()
+session_info <- devtools::session_info()
+set.seed(10)
+
+# How many cells need to be seen to count as "win"?
+min_cell_count <- 50
+earlierday_cond_str <- "day10_COCL2"
+later_cond <- "week5_COCL2"
+
+lintab <- as.data.frame.matrix(table(all_data$assigned_lineage, all_data$dataset)) # lineage by condition table
+
+sel.lineages <- rownames(lintab)[which(lintab[,later_cond] >= min_cell_count)]  # lineages that survived in given condition
+cells.1 <- which(all_data$assigned_lineage %in% sel.lineages 
+                 & all_data$assigned_posterior > 0.5
+                 & all_data$dataset == earlierday_cond_str)
+
+# compute nearest-neighbors
+set.seed(10)
+mat <- all_data[["pca"]]@cell.embeddings
+num_neigh <- 30
+nn_mat <- RANN::nn2(mat, k = num_neigh+1)$nn.idx
+
+# compute synchrony scores
+rna_common <- multiSVD_obj$common_mat_1
+multiSVD_obj <- tiltedCCA:::.set_defaultAssay(multiSVD_obj, assay = 1)
+svd_1 <- tiltedCCA:::.get_SVD(multiSVD_obj)
+multiSVD_obj <- tiltedCCA:::.set_defaultAssay(multiSVD_obj, assay = 2)
+svd_2 <- tiltedCCA:::.get_SVD(multiSVD_obj)
+tmp <- crossprod(svd_2$u, svd_1$u)
+svd_tmp <- svd(tmp)
+rotation_mat <- tcrossprod(svd_tmp$u, svd_tmp$v)
+atac_pred <- tcrossprod(tiltedCCA:::.mult_mat_vec(svd_2$u %*% rotation_mat, svd_1$d), svd_1$v)
+n <- nrow(rna_common)
+alignment_vec <- sapply(1:n, function(i){
+  if(i %% floor(n/10) == 0) cat('*')
+  
+  df <- data.frame(rna = rna_common[i,],
+                   atac = atac_pred[i,])
+  lm_res <- stats::lm(rna ~ atac, data = df)
+  summary(lm_res)$r.squared
+})
+
+# form smoothing nearest-neighbor matrix
+n <- nrow(nn_mat)
+i_vec <- rep(1:n, each = ncol(nn_mat))
+j_vec <- unlist(lapply(1:n, function(i){nn_mat[i,]}))
+avg_mat <- Matrix::sparseMatrix(i = i_vec, 
+                                j = j_vec, 
+                                x = rep(1/ncol(nn_mat), length(i_vec)), 
+                                dims = c(n,n))
+avg_mat <- Matrix::t(avg_mat)
+alignment_vec_smoothed <- as.numeric(alignment_vec %*% avg_mat)
+
+scaling_grid <- seq(0.1, 10, length.out = 100)
+scaling_quality <- sapply(scaling_grid, function(val){
+  stats::cor(alignment_vec_smoothed^val, rank(alignment_vec_smoothed))
+})
+all_data$alignment <- alignment_vec_smoothed^(scaling_grid[which.max(scaling_quality)])
+num_color <- 100
+color_palette <- viridis::viridis(num_color)
+color_breaks <- seq(min(all_data$alignment), max(all_data$alignment), length.out = num_color)
+color_vec <- sapply(all_data$alignment, function(val){
+  color_palette[which.min(abs(color_breaks - val))]
+})
+names(alignment_vec_smoothed) <- colnames(all_data)
+
+##############
+
+# plotting
+
+set.seed(10)
+Seurat::Idents(all_data) = "dataset"
+all_data <- Seurat::RunUMAP(all_data, reduction.key="ftUMAP", reduction = "fasttopic_COCL2", dims=c(1:30))
+
+png(paste0("../../../../out/figures/Writeup6c/Writeup6c_tcca-RNA-ATAC_COCL2_frontrunner-", min_cell_count, "_synchrony_nn.png"),
+    height = 2000, width = 2000, units = "px", res = 500)
+plot(x = all_data@reductions[["umap"]]@cell.embeddings[,1],
+     y = all_data@reductions[["umap"]]@cell.embeddings[,2],
+     col = color_vec, pch = 16,
+     main = paste("Cells in",earlierday_cond_str,"whose lineage has >",min_cell_count,"cells in",later_cond,": Synchrony"),
+     xaxt = "n", yaxt = "n", bty = "n",
+     xlab = "", ylab = "",
+     cex.main = 0.5)
+
+points(x = all_data@reductions[["umap"]]@cell.embeddings[cells.1,1],
+       y = all_data@reductions[["umap"]]@cell.embeddings[cells.1,2],
+       col = "white", pch = 16, cex = 3.5)
+points(x = all_data@reductions[["umap"]]@cell.embeddings[cells.1,1],
+       y = all_data@reductions[["umap"]]@cell.embeddings[cells.1,2],
+       col = color_vec[cells.1], pch = 16, cex = 2)
+graphics.off()
+
+png(paste0("../../../../out/figures/Writeup6c/Writeup6c_tcca-RNA-ATAC_COCL2_synchrony_nn.png"),
+    height = 2000, width = 2000, units = "px", res = 500)
+plot(x = all_data@reductions[["umap"]]@cell.embeddings[,1],
+     y = all_data@reductions[["umap"]]@cell.embeddings[,2],
+     col = color_vec, pch = 16,
+     main = paste("Cells in",earlierday_cond_str,"whose lineage has >",min_cell_count,"cells in",later_cond,": Synchrony"),
+     xaxt = "n", yaxt = "n", bty = "n",
+     xlab = "", ylab = "",
+     cex.main = 0.5)
+graphics.off()
+
+# plot the histogram
+
+cells.2 <- which(!all_data$assigned_lineage %in% sel.lineages 
+                 & all_data$assigned_posterior > 0.5
+                 & all_data$dataset == earlierday_cond_str)
+
+png(paste0("../../../../out/figures/Writeup6c/Writeup6c_tcca-RNA-ATAC_COCL2_synchrony-histogram_nn.png"),
+    height = 1500, width = 3000, units = "px", res = 500)
+par(mfrow = c(1,2), mar = c(4,4,4,0.5))
+
+hist(all_data$alignment[cells.1], breaks = 50, col = "gray", 
+     main = paste0("Cells in ",earlierday_cond_str," whose lineage\nhas > ",min_cell_count," cells in ",later_cond),
+     cex.main = 0.6,
+     xlim = range(all_data$alignment))
+mean_val <- mean(all_data$alignment[cells.1])
+median_val <- median(all_data$alignment[cells.1])
+lines(rep(mean_val, 2), c(-1e6,1e6), col = 2, lwd = 2, lty = 1)
+lines(rep(median_val, 2), c(-1e6,1e6), col = 3, lwd = 2, lty = 2)
+
+hist(all_data$alignment[cells.2], breaks = 50, col = "gray", 
+     main = paste0("Cells in ",earlierday_cond_str," whose lineage\nhas < ",min_cell_count," cells in ",later_cond),
+     cex.main = 0.6,
+     xlim = range(all_data$alignment))
+mean_val <- mean(all_data$alignment[cells.2])
+median_val <- median(all_data$alignment[cells.2])
+lines(rep(mean_val, 2), c(-1e6,1e6), col = 2, lwd = 2, lty = 1)
+lines(rep(median_val, 2), c(-1e6,1e6), col = 3, lwd = 2, lty = 2)
+
+graphics.off()
+
+
+set.seed(10)
+t_test <- stats::t.test(x = all_data$alignment[cells.1],
+                        y = all_data$alignment[cells.2],
+                        alternative = "two.sided")
+wilcox_test <- stats::wilcox.test(x = all_data$alignment[cells.1],
+                                  y = all_data$alignment[cells.2],
+                                  alternative = "two.sided")
+# COCL2
+t_test
+wilcox_test
+
+
---
+++ kevin/Writeup6c/Writeup6c_RNA-ATAC_DABTRAM_synchrony-fitness_scan_nn.R
@@ -0,0 +1,78 @@
+rm(list=ls())
+library(Seurat)
+library(Signac)
+load("../../../../out/kevin/Writeup6c/Writeup6c_tcca_RNA-ATAC_DABTRAM.RData")
+
+date_of_run <- Sys.time()
+session_info <- devtools::session_info()
+set.seed(10)
+
+# compute nearest-neighbors
+set.seed(10)
+mat <- all_data[["pca"]]@cell.embeddings
+num_neigh <- 30
+nn_mat <- RANN::nn2(mat, k = num_neigh+1)$nn.idx
+
+######################
+
+cell_idx <- intersect(intersect(which(all_data$dataset == "day10_DABTRAM"),
+                                which(all_data$assigned_posterior > 0.5)),
+                      which(!is.na(all_data$assigned_lineage)))
+lineage_vec <- all_data$assigned_lineage[cell_idx]
+
+tab_mat <- table(all_data$assigned_lineage, all_data$dataset)
+future_num_vec <- sapply(lineage_vec, function(lineage){
+  tab_mat[lineage,"week5_DABTRAM"]
+})
+
+# form smoothing nearest-neighbor matrix
+n <- nrow(nn_mat)
+i_vec <- rep(1:n, each = ncol(nn_mat))
+j_vec <- unlist(lapply(1:n, function(i){nn_mat[i,]}))
+avg_mat <- Matrix::sparseMatrix(i = i_vec, 
+                                j = j_vec, 
+                                x = rep(1/ncol(nn_mat), length(i_vec)), 
+                                dims = c(n,n))
+avg_mat <- Matrix::t(avg_mat)
+future_num_vec_smoothed <- as.numeric(future_num_vec %*% avg_mat)
+
+# for a gene, compute the regression line regressing alignment onto future fitness across all the cells
+target_genes <- colnames(rna_common)
+
+order_vec <- order(future_num_vec, decreasing = F)
+rna_mat <- rna_common[cell_idx[order_vec],target_genes]
+atac_mat <- atac_pred[cell_idx[order_vec],target_genes]
+
+rna_mat2 <- t(scale(t(rna_mat)))
+atac_mat2 <- t(scale(t(atac_mat)))
+diff_mat <- (rna_mat2 - atac_mat2)/rna_mat2
+colnames(diff_mat) <- colnames(rna_mat2)
+diff_mat <- abs(diff_mat[,target_genes])
+diff_mat <- pmin(diff_mat, 15)
+
+x_vec <- log1p(future_num_vec_smoothed[order_vec])
+
+p <- ncol(diff_mat); n <- nrow(diff_mat)
+pred_diff_mat <- sapply(1:p, function(j){
+  print(j)
+  
+  tmp_df <- data.frame(y = diff_mat[,j], x = x_vec)
+  reg_res <- npregfast::frfast(y ~ x, data = tmp_df)
+  y_vec <- stats::predict(object = reg_res, newdata = data.frame(x = x_vec))
+  y_vec$Estimation[,"Pred"]
+})
+colnames(pred_diff_mat) <- colnames(diff_mat)
+
+coef_vec <- sapply(1:ncol(pred_diff_mat), function(j){
+  tmp_df <- data.frame(y = diff_mat[,j], x = x_vec)
+  
+  tmp_lm <- stats::lm(y ~ x, data = tmp_df)
+  # summary(tmp_lm)$r.squared
+  stats::coef(tmp_lm)["x"]
+})
+
+save(pred_diff_mat, coef_vec, nn_mat,
+     rna_mat, atac_mat, future_num_vec, future_num_vec_smoothed,
+     date_of_run, session_info,
+     file = "../../../../out/kevin/Writeup6c/Writeup6c_RNA-ATAC_DABTRAM_synchrony-fitness_scan_nn.RData")
+
