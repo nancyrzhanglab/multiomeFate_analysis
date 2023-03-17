63360b201ab4b26e831d45310b62be9ed1d072b7
linnykos <klin1234@gmail.com>
2023-03-16 20:13:38 -0400

    Avoiding large genes for now

--- kevin/Writeup6d/Writeup6d_DABTRAM_chromatin-coverage.R
+++ kevin/Writeup6d/Writeup6d_DABTRAM_chromatin-coverage.R
@@ -10,6 +10,7 @@ source("coverage_extractor_singlecell-plotter.R")
 source("../Writeup6b/gene_list.R")
 source("gene_list_csc.R")
 gene_vec <- sort(unique(c(unlist(keygenes), keygenes_csc)))
+gene_vec <- gene_vec[74:length(gene_vec)] # TEMPORARY 
 
 tab_mat <- table(all_data$assigned_lineage, all_data$dataset)
 surviving_lineages <- rownames(tab_mat)[which(tab_mat[,"day10_DABTRAM"] >= 20)]
@@ -77,4 +78,6 @@ for(i in 1:length(gene_vec)){
     
     graphics.off()
   }
+  
+  gc()
 }
