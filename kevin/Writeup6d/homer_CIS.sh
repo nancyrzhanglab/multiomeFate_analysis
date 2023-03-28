6dcb2264f256504e7e70d4244ff5f8389f84481f
linnykos <klin1234@gmail.com>
2023-03-28 00:38:35 -0400

    Fixing typos

--- kevin/Writeup6d/homer_CIS.sh
+++ kevin/Writeup6d/homer_CIS.sh
@@ -1,5 +1,5 @@
 #!/bin/bash/
-#$ -N homer_dabtram
+#$ -N homer_cis
 #$ -j y
 #$ -o $HOME/project/Multiome_fate/out/kevin/Writeup6d/qsub
 #$ -l m_mem_free=100G
@@ -9,5 +9,5 @@ cd $HOME/project/Multiome_fate/out/kevin/Writeup6d
 module load samtools
 module load homer
 
-findMotifsGenome.pl Writeup6d_DABTRAM_differential_negpeaks.txt hg38 homer_DABTRAM_neg/ -size 200 -mask -bg Writeup6d_DABTRAM_differential_bgpeaks.txt
-findMotifsGenome.pl Writeup6d_DABTRAM_differential_pospeaks.txt hg38 homer_DABTRAM_pos/ -size 200 -mask -bg Writeup6d_DABTRAM_differential_bgpeaks.txt
+findMotifsGenome.pl Writeup6d_CIS_differential_negpeaks.txt hg38 homer_CIS_neg/ -size 200 -mask -bg Writeup6d_CIS_differential_bgpeaks.txt
+findMotifsGenome.pl Writeup6d_CIS_differential_pospeaks.txt hg38 homer_CIS_pos/ -size 200 -mask -bg Writeup6d_CIS_differential_bgpeaks.txt
