65039ba8362fcc434a38d92edf72384d38836c8c
linnykos <klin1234@gmail.com>
2023-03-30 10:17:53 -0400

    Fixing typo

--- kevin/Writeup6d/homer_DABTRAM_day10-week5.sh
+++ kevin/Writeup6d/homer_DABTRAM_day10-week5.sh
@@ -9,5 +9,5 @@ cd $HOME/project/Multiome_fate/out/kevin/Writeup6d
 module load samtools
 module load homer
 
-findMotifsGenome.pl Writeup6d_DABTRAM_day10-week5_differential_negpeaks.txt hg38 homer/homer_DABTRAM_day10-week5_neg/ -size 200 -mask -bg Writeup6d_DABTRAM_differential_day10-week5_bgpeaks.txt
-findMotifsGenome.pl Writeup6d_DABTRAM_day10-week5_differential_pospeaks.txt hg38 homer/homer_DABTRAM_day10-week5_pos/ -size 200 -mask -bg Writeup6d_DABTRAM_differential_day10-week5_bgpeaks.txt
+findMotifsGenome.pl Writeup6d_DABTRAM_day10-week5_differential_negpeaks.txt hg38 homer/homer_DABTRAM_day10-week5_neg/ -size 200 -mask -bg Writeup6d_DABTRAM_day10-week5_differential_bgpeaks.txt
+findMotifsGenome.pl Writeup6d_DABTRAM_day10-week5_differential_pospeaks.txt hg38 homer/homer_DABTRAM_day10-week5_pos/ -size 200 -mask -bg Writeup6d_DABTRAM_day10-week5_differential_bgpeaks.txt
