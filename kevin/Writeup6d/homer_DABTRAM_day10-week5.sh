a4da015a990956765e1c82e4b4ad4b54e42a7e98
linnykos <klin1234@gmail.com>
2023-03-30 10:13:41 -0400

    Pushing Homer

---
+++ kevin/Writeup6d/homer_DABTRAM_day10-week5.sh
@@ -0,0 +1,13 @@
+#!/bin/bash/
+#$ -N homer_dt_d10-w5
+#$ -j y
+#$ -o ../../../../out/kevin/Writeup6d/qsub
+#$ -l m_mem_free=100G
+
+cd $HOME/project/Multiome_fate/out/kevin/Writeup6d
+
+module load samtools
+module load homer
+
+findMotifsGenome.pl Writeup6d_DABTRAM_day10-week5_differential_negpeaks.txt hg38 homer/homer_DABTRAM_day10-week5_neg/ -size 200 -mask -bg Writeup6d_DABTRAM_differential_day10-week5_bgpeaks.txt
+findMotifsGenome.pl Writeup6d_DABTRAM_day10-week5_differential_pospeaks.txt hg38 homer/homer_DABTRAM_day10-week5_pos/ -size 200 -mask -bg Writeup6d_DABTRAM_differential_day10-week5_bgpeaks.txt
