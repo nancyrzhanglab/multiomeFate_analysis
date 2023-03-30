94bc215bdd2c57e7ff53c572095fa68e3be940c8
linnykos <klin1234@gmail.com>
2023-03-30 15:28:37 -0400

    Pushing homer for day0-week5

---
+++ kevin/Writeup6e/Writeup6e_DABTRAM_peak-entropy_experiment.R
@@ -0,0 +1,10 @@
+rm(list=ls())
+library(Seurat)
+library(Signac)
+library(GenomicRanges)
+
+load("../../../../out/kevin/Writeup6b/Writeup6b_all-data.RData")
+
+set.seed(10)
+date_of_run <- Sys.time()
+session_info <- devtools::session_info()
--- kevin/Writeup6e/peak_entropy.R
+++ kevin/Writeup6e/peak_entropy.R
@@ -1,3 +1,8 @@
-peak_mixture_modeling <- function(mat,){
+peak_mixture_modeling <- function(bin_cutoffs,
+                                  mat, # rows = cells, columns = basepairs
+                                  peak_locations,
+                                  peak_prob,
+                                  max_iter = 100,
+                                  tol = 1e-6){
   
 }
