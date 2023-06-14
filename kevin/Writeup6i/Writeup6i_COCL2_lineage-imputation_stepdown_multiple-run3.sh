bdcb0464a2a8f38648ce2cb3bd4f417459cf026f
linnykos <klin1234@gmail.com>
2023-06-14 01:08:55 -0400

    Upping memory count

--- kevin/Writeup6i/Writeup6i_COCL2_lineage-imputation_stepdown_multiple-run3.sh
+++ kevin/Writeup6i/Writeup6i_COCL2_lineage-imputation_stepdown_multiple-run3.sh
@@ -2,6 +2,6 @@
 #$ -N COCL2_imp_stpdwn3
 #$ -j y
 #$ -o ../../../../out/kevin/Writeup6i/qsub/
-#$ -l m_mem_free=25G
+#$ -l m_mem_free=100G
 
 Rscript --no-save Writeup6i_COCL2_lineage-imputation_stepdown_multiple-run3.R
