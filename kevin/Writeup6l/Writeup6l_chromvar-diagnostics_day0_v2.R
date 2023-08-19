9c78f366720aa0c6502e127036ab569c5c15b4e1
linnykos <klin1234@gmail.com>
2023-08-19 00:18:39 -0700

    Pushing lightweight

---
+++ kevin/Writeup6l/Writeup6l_chromvar-diagnostics_day0_v2.R
@@ -0,0 +1,108 @@
+rm(list=ls())
+library(Seurat)
+library(Signac)
+library(GenomicRanges)
+library(IRanges)
+library(GGally)
+library(ggplot2)
+
+load("../../../../out/kevin/Writeup6l/Writeup6l_chromVar_rna-chromvar_lightweight.RData")
+tab_mat_full <- table(all_data$assigned_lineage, all_data$dataset)
+
+load("../../../../out/kevin/Writeup6l/Writeup6l_day0_chromvar_lightweight_noATAC.RData")
+
+tab_vec <- table(all_data$assigned_lineage)
+tab_vec <- tab_vec[tab_vec > 0]
+lineage_names <- names(tab_vec)
+tab_mat_full <- tab_mat_full[lineage_names,]
+
+name_vec <- names(data.use) 
+motif_idx <- sort(unique(c(grep("JUN", name_vec),
+                           grep("FOS", name_vec),
+                           grep("NFE2", name_vec),
+                           grep("TEAD", name_vec))))
+
+# compute which cells belong to which lineage
+lineage_idx_list <- lapply(lineage_names, function(lineage){
+  intersect(which(all_data$assigned_lineage == lineage),
+            which(all_data$dataset == "day0"))
+})
+
+# for each lineage, compute the mean scores
+mean_mat <- sapply(motif_idx, function(i){
+  sapply(lineage_idx_list, function(idx_vec){
+    mean(all_data[["chromvar"]]@data[i,idx_vec])
+  })
+})
+rownames(mean_mat) <- lineage_names
+colnames(mean_mat) <- name_vec[motif_idx]
+
+max_mat <- sapply(motif_idx, function(i){
+  sapply(lineage_idx_list, function(idx_vec){
+    max(all_data[["chromvar"]]@data[i,idx_vec])
+  })
+})
+rownames(max_mat) <- lineage_names
+colnames(max_mat) <- name_vec[motif_idx]
+
+# make a ton of plots now
+n <- nrow(tab_mat_full)
+set.seed(10)
+pdf("../../../../out/figures/Writeup6l/Writeup6l_chromvar_v2-DABTRAM_ap1motif-day0size-day10size.pdf", 
+    onefile = T, width = 8, height = 8)
+for(j in 1:ncol(mean_mat)){
+  df <- data.frame(day0 = log10(tab_mat_full[,"day0"] + runif(n, min = 1, max = 1.3)),
+                   day10_dabtram = log10(tab_mat_full[,"day10_DABTRAM"] + runif(n, min = 1, max = 1.3)),
+                   week5_dabtram = log10(tab_mat_full[,"week5_DABTRAM"] + runif(n, min = 1, max = 1.3)),
+                   chromvar1 = mean_mat[,j],
+                   chromvar2 = max_mat[,j])
+  colnames(df)[4] <- paste0(colnames(mean_mat)[j], "_mean")
+  colnames(df)[5] <- paste0(colnames(max_mat)[j], "_max")
+  p <- GGally::ggpairs(df, 
+                       lower = list(continuous = GGally::wrap("points", alpha = 0.2, shape = 16)),
+                       progress = FALSE) + ggplot2::theme_bw()
+  print(p)
+}
+dev.off()
+
+###########
+
+n <- nrow(tab_mat_full)
+set.seed(10)
+pdf("../../../../out/figures/Writeup6l/Writeup6l_chromvar_v2-COCL2_ap1motif-day0size-day10size.pdf", 
+    onefile = T, width = 8, height = 8)
+for(j in 1:ncol(mean_mat)){
+  df <- data.frame(day0 = log10(tab_mat_full[,"day0"] + runif(n, min = 1, max = 1.3)),
+                   day10_cocl2 = log10(tab_mat_full[,"day10_COCL2"] + runif(n, min = 1, max = 1.3)),
+                   week5_cocl2 = log10(tab_mat_full[,"week5_COCL2"] + runif(n, min = 1, max = 1.3)),
+                   chromvar1 = mean_mat[,j],
+                   chromvar2 = max_mat[,j])
+  colnames(df)[4] <- paste0(colnames(mean_mat)[j], "_mean")
+  colnames(df)[5] <- paste0(colnames(max_mat)[j], "_max")
+  p <- GGally::ggpairs(df, 
+                       lower = list(continuous = GGally::wrap("points", alpha = 0.2, shape = 16)),
+                       progress = FALSE) + ggplot2::theme_bw()
+  print(p)
+}
+dev.off()
+
+###########
+
+n <- nrow(tab_mat_full)
+set.seed(10)
+pdf("../../../../out/figures/Writeup6l/Writeup6l_chromvar_v2-CIS_ap1motif-day0size-day10size.pdf", 
+    onefile = T, width = 8, height = 8)
+for(j in 1:ncol(mean_mat)){
+  df <- data.frame(day0 = log10(tab_mat_full[,"day0"] + runif(n, min = 1, max = 1.3)),
+                   day10_cis = log10(tab_mat_full[,"day10_CIS"] + runif(n, min = 1, max = 1.3)),
+                   week5_cis = log10(tab_mat_full[,"week5_CIS"] + runif(n, min = 1, max = 1.3)),
+                   chromvar1 = mean_mat[,j],
+                   chromvar2 = max_mat[,j])
+  colnames(df)[4] <- paste0(colnames(mean_mat)[j], "_mean")
+  colnames(df)[5] <- paste0(colnames(max_mat)[j], "_max")
+  p <- GGally::ggpairs(df, 
+                       lower = list(continuous = GGally::wrap("points", alpha = 0.2, shape = 16)),
+                       progress = FALSE) + ggplot2::theme_bw()
+  print(p)
+}
+dev.off()
---
+++ kevin/Writeup6l/Writeup6l_day0-atac_extract_lightweight.R
@@ -0,0 +1,21 @@
+rm(list=ls())
+library(Seurat)
+library(Signac)
+library(GenomicRanges)
+library(IRanges)
+
+date_of_run <- Sys.time()
+session_info <- devtools::session_info()
+set.seed(10)
+
+load("../../../../out/kevin/Writeup6l/Writeup6l_day0-atac_extract.RData")
+
+keep_vec <- rep(FALSE, ncol(data))
+keep_vec[sample(1:length(keep_vec), 100)] <- TRUE
+all_data$keep <- keep_vec
+all_data <- subset(all_data, keep == TRUE)
+
+print("Saving")
+save(date_of_run, session_info, 
+     all_data, 
+     file = "../../../../out/kevin/Writeup6l/Writeup6l_day0-atac_extract_lightweight.RData")
