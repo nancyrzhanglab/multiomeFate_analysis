4db0a90c8ca5b3ea4817cd5289531658ac45b117
linnykos <klin1234@gmail.com>
2023-08-19 01:58:44 -0700

    Another minor change

--- kevin/Writeup6l/Writeup6l_chromvar_day0_chromvar.R
+++ kevin/Writeup6l/Writeup6l_chromvar_day0_chromvar.R
@@ -98,3 +98,24 @@ all_data <- Signac::RunChromVAR(
 save(date_of_run, session_info, 
      all_data, motif.matrix, motif.positions, motif,
      file = "../../../../out/kevin/Writeup6l/Writeup6l_chromvar_day0.RData")
+
+#############################
+
+print("Extracting motifs")
+data.use <- Signac::GetMotifData(object = all_data,
+                                 assay = "ATAC",
+                                 slot = "pwm")
+names(data.use) <- Signac::GetMotifData(
+  object = all_data,
+  assay = "ATAC",
+  slot = "motif.names"
+)
+
+Seurat::DefaultAssay(all_data) <- "Saver"
+all_data[["ATAC"]] <- NULL
+
+save(date_of_run, session_info, 
+     all_data, data.use,
+     file = "../../../../out/kevin/Writeup6l/Writeup6l_day0_chromvar_lightweight_noATAC.RData")
+
+
--- kevin/Writeup6l/Writeup6l_chromvar_day0_chromvar_step2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#$ -N day0-chromvar_step2
-#$ -j y
-#$ -o ../../../../out/kevin/Writeup6l/qsub/
-#$ -l m_mem_free=100G
-
-Rscript --no-save Writeup6l_chromvar_day0_chromvar_step2.R
