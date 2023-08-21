23b6f6a15b6646748c5e29cf004befd3ab7d9797
linnykos <klin1234@gmail.com>
2023-08-21 00:06:29 -0700

    Fixing a really careless typo

--- kevin/Writeup6l/Writeup6l_differential_peak_COCL2_split-by-day0_forHomer.sh
+++ kevin/Writeup6l/Writeup6l_differential_peak_COCL2_split-by-day0_forHomer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#$ -N day0-atac-subset_lw
+#$ -N diff-peak_cocl2_day0
 #$ -j y
 #$ -o ../../../../out/kevin/Writeup6l/qsub/
 #$ -l m_mem_free=100G
 
-Rscript --no-save Writeup6l_day0-atac_extract_lightweight.R
+Rscript --no-save Writeup6l_differential_peak_COCL2_split-by-day0_forHomer.R
