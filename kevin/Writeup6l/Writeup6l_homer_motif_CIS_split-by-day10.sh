9069c9cea6fe9b38f43490290fd1666ba9065df5
linnykos <klin1234@gmail.com>
2023-08-23 22:08:11 -0700

    Pushing homer analyses

---
+++ kevin/Writeup6l/Writeup6l_homer_motif_CIS_split-by-day10.sh
@@ -0,0 +1,13 @@
+#!/bin/bash/
+#$ -N homer_cis_day10
+#$ -j y
+#$ -o ../../../../out/kevin/Writeup6l/qsub
+#$ -l m_mem_free=100G
+
+cd $HOME/project/Multiome_fate/out/kevin/Writeup6l
+
+module load samtools
+module load homer
+
+findMotifsGenome.pl Writeup6l_CIS-split-by-day10_differential_negpeaks.txt hg38 homer/homer_CIS-split-by-day10_neg/ -size 200 -mask -bg Writeup6l_CIS-split-by-day10_differential_bgpeaks.txt
+findMotifsGenome.pl Writeup6l_CIS-split-by-day10_differential_pospeaks.txt hg38 homer/homer_CIS-split-by-day10_pos/ -size 200 -mask -bg Writeup6l_CIS-split-by-day10_differential_bgpeaks.txt
