057b1acd156999f87ebdb45dea9be8a5e988dd50
linnykos <klin1234@gmail.com>
2023-08-30 23:53:35 -0700

    Pushing differential peak analysis within signac

---
+++ kevin/Writeup6l/Writeup6l_day0_atac_NN_chromvar-smoothing.R
@@ -0,0 +1,11 @@
+rm(list=ls())
+library(Seurat)
+library(Signac)
+library(GenomicRanges)
+library(IRanges)
+
+date_of_run <- Sys.time()
+session_info <- devtools::session_info()
+set.seed(10)
+
+load("../../../../out/kevin/Writeup6l/Writeup6l_day0_atac_NN.RData")
