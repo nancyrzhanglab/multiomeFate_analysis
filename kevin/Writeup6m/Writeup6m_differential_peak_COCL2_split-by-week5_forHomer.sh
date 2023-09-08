3562a21714374c4387f9b60280bd8092dc05a3ea
linnykos <klin1234@gmail.com>
2023-09-07 18:46:57 -0700

    Fixing another typo

--- kevin/Writeup6m/Writeup6m_differential_peak_COCL2_split-by-week5_forHomer.sh
+++ kevin/Writeup6m/Writeup6m_differential_peak_COCL2_split-by-week5_forHomer.sh
@@ -4,4 +4,4 @@
 #$ -o ../../../../out/kevin/Writeup6m/qsub/
 #$ -l m_mem_free=100G
 
-Rscript --no-save Writeup6l_differential_peak_COCL2_split-by-day10_forHomer_part2.R
+Rscript --no-save Writeup6m_differential_peak_COCL2_split-by-week5_forHomer.R
