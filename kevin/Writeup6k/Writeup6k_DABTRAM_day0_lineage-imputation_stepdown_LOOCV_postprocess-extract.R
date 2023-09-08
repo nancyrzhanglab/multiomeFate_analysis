915bceb142d55cc39deefa0bc07e91b99ac1ca3c
linnykos <klin1234@gmail.com>
2023-09-08 08:54:15 -0700

    Minor change

--- kevin/Writeup6k/Writeup6k_DABTRAM_day0_lineage-imputation_stepdown_LOOCV_postprocess-extract.R
+++ kevin/Writeup6k/Writeup6k_DABTRAM_day0_lineage-imputation_stepdown_LOOCV_postprocess-extract.R
@@ -3,7 +3,6 @@ rm(list=ls())
 library(Seurat)
 library(Signac)
 library(GenomicRanges)
-library(multiomeFate)
 library(IRanges)
 
 load("../../../../out/kevin/Writeup6k/Writeup6k_DABTRAM_day0_lineage-imputation_stepdown-LOOCV_postprocessed.RData")
