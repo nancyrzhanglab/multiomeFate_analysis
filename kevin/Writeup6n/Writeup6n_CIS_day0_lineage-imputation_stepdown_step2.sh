24e48d972199924641e4db956bd9ffcd69b090f6
linnykos <klin1234@gmail.com>
2023-09-28 11:37:31 -0700

    one more file

---
+++ kevin/Writeup6n/Writeup6n_CIS_day0_lineage-imputation_stepdown_step2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#$ -N cis_d0_imputation-stepdown_step2
+#$ -j y
+#$ -o ../../../../out/kevin/Writeup6n/qsub/
+#$ -l m_mem_free=50G
+
+Rscript --no-save Writeup6n_CIS_day0_lineage-imputation_stepdown_step2.R
