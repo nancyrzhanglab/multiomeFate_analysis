abd37cb60d2ab439f6aca44f14e3aed0d4949194
linnykos <klin1234@gmail.com>
2023-09-28 11:35:59 -0700

    Pushing step2 of cis day0

--- kevin/Writeup6n/Writeup6n_lineage-imputation_day0-comparison.R
+++ kevin/Writeup6n/Writeup6n_lineage-imputation_day0-comparison.R
@@ -5,4 +5,28 @@ library(GenomicRanges)
 library(multiomeFate)
 library(IRanges)
 
-load("../../../../out/kevin/Writeup6k/Writeup6k_DABTRAM_day0_lineage-imputation_stepdown-LOOCV_postprocessed.RData")
+load("../../../../out/kevin/Writeup6j/Writeup6j_DABTRAM_day0_lineage-imputation_stepdown-LOOCV_concise-postprocessed.RData")
+dabtram_imputed <- cell_imputed_count
+load("../../../../out/kevin/Writeup6j/Writeup6j_COCL2_day0_lineage-imputation_stepdown-LOOCV_concise-postprocessed.RData")
+cocl2_imputed <- cell_imputed_count
+
+cell_names <- intersect(names(dabtram_imputed), names(cocl2_imputed))
+dabtram_imputed <- dabtram_imputed[cell_names]
+cocl2_imputed <- cocl2_imputed[cell_names]
+any(is.na(dabtram_imputed)); any(is.na(cocl2_imputed))
+
+# dabtram_imputed isn't log10
+dabtram_imputed <- log10(exp(dabtram_imputed))
+
+stats::cor(dabtram_imputed, cocl2_imputed)
+stats::cor(10^dabtram_imputed, 10^cocl2_imputed)
+
+df <- data.frame(cocl2_day0 = cocl2_imputed,
+                 dabtram_day0 = dabtram_imputed)
+p1 <- GGally::ggpairs(df, 
+                     lower = list(continuous = GGally::wrap("points", alpha = 0.2, shape = 16)),
+                     progress = FALSE) + ggplot2::theme_bw()
+ggplot2::ggsave(filename = "../../../../out/figures/Writeup6n/Writeup6n_lineage-imputation_day0-comparison.png",
+                p1, device = "png", width = 6, height = 6, units = "in")
+
+
