5c095e5d24802edf15dcfe6adc84b5dad874a635
Xinyi Chen <emiliac0717@gmail.com>
2023-10-04 12:08:24 -0400

    visualize individual genes

--- emilia/task_identify_features_predictive_growth_and_lineage_specific/comp_day0_exp_at_adaptation_lineage_specific_genes.R
+++ emilia/task_identify_features_predictive_growth_and_lineage_specific/comp_day0_exp_at_adaptation_lineage_specific_genes.R
@@ -1,14 +1,15 @@
 library(tidyverse)
+library(data.table)
 
 dir <- '~/Dropbox/Thesis/Lineage_trace/data/Shaffer_lab/'
-future_treatment <- 'day10_DABTRAM'
+future_treatment <- 'day10_CIS'
 # ==============================================================================
 # Read data
 # ==============================================================================
 
 # growth potential
-load(paste0(dir, 'day10_DABTRAM/Writeup6k_DABTRAM_day10_lineage-imputation_stepdown_extract-growth-potential.RData'))
-growth_potential_use <- day10_forDABTRAM_growth_potential
+load(paste0(dir, future_treatment, '/Writeup6n_CIS_day10_lineage-imputation_stepdown_concise-postprocessed.RData'))
+growth_potential_use <- cell_imputed_count
 growth_potential_use <- growth_potential_use[!is.na(growth_potential_use)]
 
 # adaptation genes
@@ -22,7 +23,7 @@ metadata_day0 <- read.csv(paste0(dir, 'day0/day0_meta.csv'), row.names = 1)
 metadata_day0$cell_id <- rownames(metadata_day0)
 
 # day10 data
-metadata_day10 <- read.csv(paste0(dir, 'day10_DABTRAM/day10_DABTRAM_meta.csv'), row.names = 1)
+metadata_day10 <- read.csv(paste0(dir, future_treatment, '/', future_treatment, '_meta.csv'), row.names = 1)
 metadata_day10$cell_id <- rownames(metadata_day10)
 
 # ==============================================================================
@@ -73,6 +74,48 @@ data_others$feature <- rownames(data_others)
 data_winners <- data_winners[, c('feature', 'winners_mean')]
 data_others <- data_others[, c('feature','others_mean')]
 
+
+# =====================================
+# looking at raw data
+# =====================================
+data_winners_all <- as.data.frame(data[, winners_df$cell_id])
+data_others_all <- as.data.frame(data[, others_df$cell_id])
+
+data_winners_all <- as.data.frame(t(data_winners_all))
+data_others_all <- as.data.frame(t(data_others_all))
+
+data_winners_all$category <- 'winners'
+data_others_all$category <- 'others'
+
+data_all <- as.data.frame(rbind(data_winners_all, data_others_all))
+
+adaptation_genes <- adaptation_genes[adaptation_genes$gene %in% colnames(data_all), ]
+adaptation_genes$abs_correlation <- abs(adaptation_genes$correlation)
+adaptation_genes$corr_dir <- ifelse(adaptation_genes$correlation > 0, 'Pos', 'None')
+adaptation_genes$corr_dir <- ifelse(adaptation_genes$correlation < 0, 'Neg', adaptation_genes$corr_dir)
+adaptation_genes <- adaptation_genes %>%
+  arrange(desc(abs_correlation))
+top_adaptation_genes <- head(adaptation_genes, 20)
+bottom_adaptation_genes <- tail(adaptation_genes, 5)
+other_genes <- adaptation_genes %>% 
+  filter(! gene %in% top_adaptation_genes$gene) %>% 
+  filter(! gene %in% bottom_adaptation_genes$gene) %>% 
+  sample_n(10)
+
+top_adaptation_genes$gene_category <- 'Top'
+bottom_adaptation_genes$gene_category <- 'Bottom'
+# other_genes$gene_category <- 'Others'
+
+# genes_to_check <- rbind(top_adaptation_genes, bottom_adaptation_genes, other_genes)
+genes_to_check <- top_adaptation_genes
+
+data_to_check <- data_all[, c(genes_to_check$gene, 'category')]
+data_to_check$cell_id <- row.names(data_to_check)
+data_to_check <- melt(data_to_check, id_vars=c('cell_id', 'category'))
+colnames(data_to_check) <- c('cell_lineage_category', 'cell_id', 'gene', 'RNA_SAVER')
+
+data_to_check <- merge(data_to_check, genes_to_check, by='gene')
+
 # =====================================
 # calculate the difference in expression level between winner and other cells
 # =====================================
@@ -93,5 +136,14 @@ results <- results[results$winner_minus_others != 1, ]
 ggplot(results, aes(x = factor(corr_dir, levels = c('Pos', 'Neg', 'Others')), y=winner_minus_others)) +
   geom_boxplot(outlier.alpha = 0) +
   geom_jitter(alpha=0.3, size=1) +
+  xlab('Correlation direction') +
+  ylim(c(-0.4, 0.4)) +
   theme_bw()
 
+
+ggplot(data_to_check, aes(x = reorder(gene, -abs_correlation), y = RNA_SAVER, fill=cell_lineage_category, color=corr_dir)) +
+  geom_boxplot(lwd=0.8) +
+  scale_fill_manual(values=c("#A7C7E7", "#ff0000")) +
+  scale_color_manual(values=c("#A9A9A9", "#50C878")) +
+  theme_bw() + theme(axis.text.x = element_text(angle = 60, vjust = 0.2, hjust=0.2)) +
+  coord_cartesian(ylim = c(-1, 1))
