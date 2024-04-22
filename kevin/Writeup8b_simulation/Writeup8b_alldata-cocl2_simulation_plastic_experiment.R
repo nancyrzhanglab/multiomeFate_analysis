cedeaa02c64e2c77ad0e632c3d1d2788be221b8f
linnykos <kzlin@uw.edu>
2024-04-22 01:33:15 -0700

    Got it somewhat reasonably working surprisingly

--- kevin/Writeup8b_simulation/Writeup8b_alldata-cocl2_simulation_plastic_experiment.R
+++ kevin/Writeup8b_simulation/Writeup8b_alldata-cocl2_simulation_plastic_experiment.R
@@ -4,17 +4,16 @@ library(multiomeFate)
 
 load("../../out/Writeup8b/Writeup8b_simulation_day10-COCL2.RData")
 
-par(mfrow = c(1,1))
-Seurat::DimPlot(all_data,
-                group.by = "dataset",
-                reduction = "umap")
+# par(mfrow = c(1,1))
+# Seurat::DimPlot(all_data,
+#                 group.by = "dataset",
+#                 reduction = "umap")
 
 set.seed(10)
 embedding_mat <- all_data[["fasttopic_COCL2"]]@cell.embeddings
 embedding_mat <- scale(embedding_mat)
-embedding_mat <- pmin(embedding_mat, 2)
-coefficient_intercept <- -1
-embedding_coefficient_vec <- stats::runif(ncol(embedding_mat), min = -0.5, max = 0.25) # rep(1, ncol(embedding_mat))
+coefficient_intercept <- -10
+embedding_coefficient_vec <- c(2, rep(0, ncol(embedding_mat)-1))
 
 # double-check the fate potentials are ok
 tmp <- exp((embedding_mat %*% embedding_coefficient_vec) + coefficient_intercept)
@@ -36,6 +35,8 @@ simulation_res <- multiomeFate:::generate_simulation_plastic(
   num_lineages = num_lineages,
   verbose = 3
 )
+print(head(simulation_res$lineage_assignment[rownames(simulation_res$prob_mat)]))
+
 
 # check the simulation to make the sizes look alright
 table(simulation_res$lineage_assignment)
@@ -54,12 +55,15 @@ par(mfrow = c(1,1))
 plot(all_data[["umap"]]@cell.embeddings,
      pch = 16, col = "gray",
      xlab = "UMAP1", ylab = "UMAP2")
-for(kk in 1:3){
-  idx <- which(simulation_res$lineage_assignment == paste0("lineage:", kk))
+lineage_idx_vec <- c(1,49,50)
+for(kk in 1:length(lineage_idx_vec)){
+  idx <- which(simulation_res$lineage_assignment == paste0("lineage:", lineage_idx_vec[kk]))
   points(all_data[["umap"]]@cell.embeddings[early_idx[idx],,drop = FALSE],
          pch = 16, col = kk, cex = 1)
 }
 
+
+# Version 1
 par(mfrow = c(1,3))
 median_val = sapply(levels(simulation_res$lineage_assignment), function(lineage){
   median(simulation_res$cell_fate_potential_truth[simulation_res$lineage_assignment == lineage])
@@ -67,7 +71,7 @@ median_val = sapply(levels(simulation_res$lineage_assignment), function(lineage)
 plot(median_val, 
      simulation_res$lineage_future_size, 
      pch = 16,
-     main = paste0("Future lineage size vs. current mean lineage potential\n",
+     main = paste0("Future lineage size vs. current median lineage potential\n",
                    "Corr: ", round(cor(median_val, simulation_res$lineage_future_size), 2)))
 range_val = sapply(levels(simulation_res$lineage_assignment), function(lineage){
   diff(range(simulation_res$cell_fate_potential_truth[simulation_res$lineage_assignment == lineage]))
@@ -75,12 +79,36 @@ range_val = sapply(levels(simulation_res$lineage_assignment), function(lineage){
 plot(range_val, 
      simulation_res$lineage_future_size, 
      pch = 16,
-     main = paste0("Future lineage size vs. current sd lineage potential\n",
+     main = paste0("Future lineage size vs. current range lineage potential\n",
                    "Corr: ", round(cor(range_val, simulation_res$lineage_future_size), 2)))
 plot(x = median_val,
      y = range_val, 
+     xlab = "median", ylab = "range",
+     pch = 16, asp = TRUE)
+
+# Version 2
+par(mfrow = c(1,3))
+mean_val = sapply(levels(simulation_res$lineage_assignment), function(lineage){
+  median(simulation_res$cell_fate_potential_truth[simulation_res$lineage_assignment == lineage])
+})
+plot(mean_val, 
+     simulation_res$lineage_future_size, 
+     pch = 16,
+     main = paste0("Future lineage size vs. current mean lineage potential\n",
+                   "Corr: ", round(cor(median_val, simulation_res$lineage_future_size), 2)))
+sd_val = sapply(levels(simulation_res$lineage_assignment), function(lineage){
+  diff(range(simulation_res$cell_fate_potential_truth[simulation_res$lineage_assignment == lineage]))
+})
+plot(sd_val, 
+     simulation_res$lineage_future_size, 
+     pch = 16,
+     main = paste0("Future lineage size vs. current sd lineage potential\n",
+                   "Corr: ", round(cor(range_val, simulation_res$lineage_future_size), 2)))
+plot(x = mean_val,
+     y = sd_val, 
      xlab = "mean", ylab = "sd",
      pch = 16, asp = TRUE)
+
 
 #####
 
@@ -126,6 +154,7 @@ p1 <- p1 + ggplot2::stat_summary(fun=max, geom="point", shape=10, size=5, color=
 p1 <- p1 + ggplot2::ggtitle(paste0("ANOVA -Log10(pvalue)=", round(-log10(anova_res$p.value), 2), ", Lineage effect = ", lineage_effect, "%"))
 p1
 
+
 ##################
 
 # try out our fate potential method
@@ -141,6 +170,7 @@ colnames(tab_mat) <- c("now", "future")
 #################
 # start cross validation
 
+set.seed(10)
 fit_res <- multiomeFate:::lineage_cv(
   cell_features = embedding_mat[early_idx,,drop=FALSE],
   cell_lineage = cell_lineage,
@@ -153,7 +183,7 @@ fit_res <- multiomeFate:::lineage_cv(
   verbose = 2
 )
 
-final_fit <- lineage_cv_finalize(
+final_fit <- multiomeFate:::lineage_cv_finalize(
   cell_features = embedding_mat[early_idx,,drop=FALSE],
   cell_lineage = cell_lineage,
   fit_res = fit_res,
@@ -161,11 +191,11 @@ final_fit <- lineage_cv_finalize(
 )
 lineage_imputed_count <- final_fit$lineage_imputed_count
 cell_imputed_score <- final_fit$cell_imputed_score
-round(final_fit$embedding_coefficient_vec, 2)
+round(final_fit$coefficient_vec, 2)
 
 par(mfrow = c(1,1))
 plot(x = c(coefficient_intercept, embedding_coefficient_vec),
-     y = final_fit$embedding_coefficient_vec,
+     y = final_fit$coefficient_vec,
      xlab = "True coefficients",
      ylab = "Estimated coefficients",
      asp = TRUE, pch = 16)
@@ -192,4 +222,6 @@ plot(x = simulation_res$cell_fate_potential_truth,
        cell_imputed_score[names(simulation_res$cell_fate_potential_truth)]
      ), 2))
 )
+
+
 
