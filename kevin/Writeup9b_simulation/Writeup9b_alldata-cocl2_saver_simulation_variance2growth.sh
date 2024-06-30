2ba03bf903b3b048ca10edbc0a406f48ef62a697
linnykos <kzlin@uw.edu>
2024-06-29 23:42:05 -0700

    Pushing fixes

--- kevin/Writeup9b_simulation/Writeup9b_alldata-cocl2_saver_simulation_variance2growth.sh
+++ kevin/Writeup9b_simulation/Writeup9b_alldata-cocl2_saver_simulation_variance2growth.sh
@@ -4,4 +4,4 @@
 #$ -o ../../../../out/kevin/Writeup9b/qsub/
 #$ -l m_mem_free=30G
 
-Rscript --no-save Writeup9b_alldata-cocl2_saver-pca_simulation_variance2growth.R
+Rscript --no-save Writeup9b_alldata-cocl2_saver_simulation_variance2growth.R
