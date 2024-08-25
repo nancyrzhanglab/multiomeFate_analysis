d8688c153852607617b9ad82bdf129cdeef9b8ed
linnykos <kzlin@uw.edu>
2024-08-24 21:45:32 -0700

    Trying to debug

--- kevin/Writeup10a_final-dylan_preprocess/Writeup10a_ppStep3_combine.R
+++ kevin/Writeup10a_final-dylan_preprocess/Writeup10a_ppStep3_combine.R
@@ -12,6 +12,9 @@ out_folder <- "/home/stat/nzh/team/kevinl1/project/Multiome_fate/out/kevin/Write
 load(paste0(out_folder, "Writeup10a_ppStep1_peakmerging.RData"))
 load(paste0(out_folder, "Writeup10a_ppStep2_rna-merge.RData"))
 
+print(all_data_atac)
+print(all_data_rna)
+
 atac_cells <- Seurat::Cells(all_data_atac)
 rna_cells <- Seurat::Cells(all_data_rna)
 common_cells <- intersect(atac_cells, rna_cells)
@@ -19,6 +22,9 @@ common_cells <- intersect(atac_cells, rna_cells)
 all_data_atac <- subset(all_data_atac, cells = common_cells)
 all_data_rna <- subset(all_data_rna, cells = common_cells)
 gc(TRUE)
+
+print(all_data_atac)
+print(all_data_rna)
 
 atac_cells <- Seurat::Cells(all_data_atac)
 rna_cells <- Seurat::Cells(all_data_rna)
@@ -33,6 +39,7 @@ rm(list = c("all_data_atac", "all_data_rna")); gc(TRUE)
 print(table(all_data$dataset))
 print(head(all_data@meta.data))
 
+print("Saving")
 save(all_data, date_of_run, session_info,
      file = paste0(out_folder, "Writeup10a_ppStep3_combine.RData"))
 
