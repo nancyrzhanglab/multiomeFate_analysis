1b0f1620640a00a2c53345daecefd939b1d5e4ea
linnykos <kzlin@uw.edu>
2024-08-24 21:32:55 -0700

    Pushing the combination script

---
+++ kevin/Writeup10a_final-dylan_preprocess/Writeup10a_ppStep3_combine.R
@@ -0,0 +1,41 @@
+rm(list=ls())
+library(Seurat)
+library(Signac)
+library(EnsDb.Hsapiens.v86)
+
+set.seed(10)
+date_of_run <- Sys.time()
+session_info <- devtools::session_info()
+
+print("Combine both dataset")
+out_folder <- "/home/stat/nzh/team/kevinl1/project/Multiome_fate/out/kevin/Writeup10a/"
+load(paste0(out_folder, "Writeup10a_ppStep1_peakmerging.RData"))
+load(paste0(out_folder, "Writeup10a_ppStep2_rna-merge.RData"))
+
+atac_cells <- Seurat::Cells(all_data_atac)
+rna_cells <- Seurat::Cells(all_data_rna)
+common_cells <- intersect(atac_cells, rna_cells)
+
+all_data_atac <- subset(all_data_atac, cells = common_cells)
+all_data_rna <- subset(all_data_atac, cells = common_cells)
+gc(TRUE)
+
+atac_cells <- Seurat::Cells(all_data_atac)
+rna_cells <- Seurat::Cells(all_data_rna)
+print(head(atac_cells))
+print(head(rna_cells))
+all(atac_cells == rna_cells)
+
+all_data_atac[["RNA"]] <- all_data_rna
+all_data <- all_data_atac
+rm(list = c("all_data_atac", "all_data_rna")); gc(TRUE)
+
+print(table(all_data$dataset))
+print(head(all_data@meta.data))
+
+save(all_data, date_of_run, session_info,
+     file = paste0(out_folder, "Writeup10a_ppStep3_combine.RData"))
+
+
+
+
--- kevin/Writeup10a_final-dylan_preprocess/Writeup10a_ppStep4_qc.R
+++ kevin/Writeup10a_final-dylan_preprocess/Writeup10a_ppStep4_qc.R
@@ -1,9 +1,3 @@
-rm(list=ls())
-library(Seurat)
-library(Signac)
-library(EnsDb.Hsapiens.v86)
-
-load("../../../../out/kevin/Writeup4e/Writeup4e_timeAll_peakmerging.RData")
 
 print("Seurat preprocess of RNA")
 Seurat::DefaultAssay(all_data) <- "RNA"
@@ -43,50 +37,12 @@ hk_genes <- read.csv("~/project/eSVD/data/housekeeping/housekeeping.txt", header
 cycling_genes <- c(cc.genes$s.genes, cc.genes$g2m.genes)
 all_genes <- unique(c(unlist(var_list), jackpot_genes, hk_genes, cycling_genes))
 all_genes <- intersect(all_genes, rownames(all_data))
-
 all_data[["RNA"]]@var.features <- all_genes
-save(all_data, date_of_run, session_info,
-     file = "../../../../out/kevin/Writeup4e/Writeup4e_timeAll_peakmerging.RData")
 
 all_data <- Seurat::NormalizeData(all_data)
-all_data <- Seurat::ScaleData(all_data)
-all_data <- Seurat::RunPCA(all_data, verbose = FALSE) 
-set.seed(10)
-all_data <- Seurat::RunUMAP(all_data, dims = 1:50)
 all_data <- Seurat::CellCycleScoring(all_data, 
                                      g2m.features = cc.genes$g2m.genes, 
                                      s.features = cc.genes$s.genes)
-
-print("Seurat preprocess of ATAC")
-Seurat::DefaultAssay(all_data) <- "ATAC"
-set.seed(10)
-all_data <- Signac::RunTFIDF(all_data)
-all_data <- Signac::FindTopFeatures(all_data, min.cutoff = 'q0')
-all_data <- Signac::RunSVD(all_data)
-set.seed(10)
-all_data <- Seurat::RunUMAP(object = all_data, 
-                            reduction = 'lsi', dims = 2:50,
-                            reduction.name = 'atac.umap')
-
-print("Seurat preprocess of gene activity")
-Seurat::DefaultAssay(all_data) <- "ATAC"
-gene_activities <- Signac::GeneActivity(all_data)
-all_data[["geneActivity"]] <- CreateAssayObject(counts = gene_activities)
-Seurat::DefaultAssay(all_data) <- "geneActivity"
-all_data[["geneActivity"]]@var.features <- intersect(rownames(gene_activities), all_data[["RNA"]]@var.features)
-all_data <- Seurat::NormalizeData(
-  object = all_data,
-  assay = "geneActivity",
-  normalization.method = "LogNormalize",
-  scale.factor = median(all_data$nCount_geneActivity)
-)
-all_data <- Seurat::ScaleData(all_data)
-all_data <- Seurat::RunPCA(all_data, verbose = FALSE,
-                           reduction.name = "activityPCA") 
-set.seed(10)
-all_data <- Seurat::RunUMAP(all_data, dims = 2:50,
-                            reduction = "activityPCA", 
-                            reduction.name = "activity.umap")
 
 print("Finished preprocessing data")
 save(all_data, date_of_run, session_info,
