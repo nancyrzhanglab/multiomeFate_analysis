dccab24f67876d60b75d8ecab24d6c604c81756e
linnykos <kzlin@uw.edu>
2024-08-25 01:48:38 -0700

    More scripts

---
+++ kevin/Writeup10a_final-dylan_preprocess/Writeup10a_ppStep7c_peakvi2_COCL2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -N peakvi_cocl2
+#$ -j y
+#$ -o ../../../../out/kevin/Writeup10a/qsub/
+#$ -l m_mem_free=50G
+
+conda activate scvi
+
+conda env list
+python --version
+conda list
+
+python $HOME/project/Multiome_fate/git/multiomeFate_analysis_kevin/kevin/Writeup10a_final-dylan_preprocess/Writeup10a_ppStep7c_peakvi2_COCL2.py
