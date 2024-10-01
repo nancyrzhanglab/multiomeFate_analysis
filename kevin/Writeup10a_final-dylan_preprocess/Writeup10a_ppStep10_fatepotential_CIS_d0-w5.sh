d842b6d0654de34780e8e376e0a156590b2c6a1d
linnykos <kzlin@uw.edu>
2024-09-30 22:48:56 -0700

    Adding the fate potential for day0 to w5

---
+++ kevin/Writeup10a_final-dylan_preprocess/Writeup10a_ppStep10_fatepotential_CIS_d0-w5.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#$ -N step10_cis-d0-w5
+#$ -j y
+#$ -o ../../../../out/kevin/Writeup10a/qsub/
+#$ -l m_mem_free=20G
+
+Rscript --no-save Writeup10a_ppStep10_fatepotential_CIS_d0-w5.R
--- kevin/Writeup10a_final-dylan_preprocess/Writeup10a_ppStep8c_saver-pca.R
+++ kevin/Writeup10a_final-dylan_preprocess/Writeup10a_ppStep8c_saver-pca.R
@@ -38,9 +38,41 @@ for(treatment in treatment_vec){
   all_data[[paste0("Saver.", treatment, ".pca")]] <- Seurat::CreateDimReducObject(mat_full)
 }
 
+dataset_vec <- unique(all_data$dataset)
+
+for(dataset in dataset_vec){
+  print(paste0("Working on ", dataset))
+  all_data2 <- subset(all_data, dataset == dataset)
+  
+  Seurat::DefaultAssay(all_data2) <- "Saver"
+  all_data2 <- Seurat::NormalizeData(all_data2)
+  all_data2 <- Seurat::FindVariableFeatures(all_data2)
+  all_data2 <- Seurat::ScaleData(all_data2)
+  all_data2 <- Seurat::RunPCA(all_data2, 
+                              verbose = FALSE,
+                              reduction.name = paste0("Saver.", dataset, ".pca"),
+                              reduction.key = paste0("Saver", dataset, "PC_"))
+  
+  mat <- all_data2[[paste0("Saver.", dataset, ".pca")]]@cell.embeddings
+  cell_names <- Seurat::Cells(all_data)
+  mat_full <- matrix(NA, nrow = length(cell_names), ncol = ncol(mat))
+  colnames(mat_full) <- colnames(mat)
+  rownames(mat_full) <- cell_names
+  mat_full[rownames(mat),] <- mat
+  all_data[[paste0("Saver.", dataset, ".pca")]] <- Seurat::CreateDimReducObject(mat_full)
+}
+
 all_data_saver_CIS_pca <- all_data[["Saver.CIS.pca"]]
 all_data_saver_COCL2_pca <- all_data[["Saver.COCL2.pca"]]
 all_data_saver_DABTRAM_pca <- all_data[["Saver.DABTRAM.pca"]]
+
+all_data_saver_day0_pca <- all_data[["Saver.day0.pca"]]
+all_data_saver_day10_CIS_pca <- all_data[["Saver.day10_CIS.pca"]]
+all_data_saver_day10_COCL2_pca <- all_data[["Saver.day10_COCL2.pca"]]
+all_data_saver_day10_DABTRAM_pca <- all_data[["Saver.day10_DABTRAM.pca"]]
+all_data_saver_week5_CIS_pca <- all_data[["Saver.week5_CIS.pca"]]
+all_data_saver_week5_COCL2_pca <- all_data[["Saver.week5_COCL2.pca"]]
+all_data_saver_week5_DABTRAM_pca <- all_data[["Saver.week5_DABTRAM.pca"]]
 
 save(all_data_saver_CIS_pca, date_of_run, session_info,
      file = paste0(out_folder, "Writeup10a_data_saver_CIS_pca.RData"))
@@ -49,5 +81,19 @@ save(all_data_saver_COCL2_pca, date_of_run, session_info,
 save(all_data_saver_DABTRAM_pca, date_of_run, session_info,
      file = paste0(out_folder, "Writeup10a_data_saver_DABTRAM_pca.RData"))
 
+save(all_data_saver_day0_pca, date_of_run, session_info,
+     file = paste0(out_folder, "Writeup10a_data_saver_day0_pca.RData"))
+save(all_data_saver_day10_CIS_pca, date_of_run, session_info,
+     file = paste0(out_folder, "Writeup10a_data_saver_day10_CIS_pca.RData"))
+save(all_data_saver_day10_COCL2_pca, date_of_run, session_info,
+     file = paste0(out_folder, "Writeup10a_data_saver_day10_COCL2_pca.RData"))
+save(all_data_saver_day10_DABTRAM_pca, date_of_run, session_info,
+     file = paste0(out_folder, "Writeup10a_data_saver_day10_DABTRAM_pca.RData"))
+save(all_data_saver_week5_CIS_pca, date_of_run, session_info,
+     file = paste0(out_folder, "Writeup10a_data_saver_week5_CIS_pca.RData"))
+save(all_data_saver_week5_COCL2_pca, date_of_run, session_info,
+     file = paste0(out_folder, "Writeup10a_data_saver_week5_COCL2_pca.RData"))
+save(all_data_saver_week5_DABTRAM_pca, date_of_run, session_info,
+     file = paste0(out_folder, "Writeup10a_data_saver_week5_DABTRAM_pca.RData"))
 
 print("Done! :)")
